5c9a5ecb7c4b7b44219ce0a5cd5ccd2655562c7b
Mahatthana <mahatthana.i@ku.th>
2022-10-19 20:06:34 +0700

    latest

--- src/main/java/ku/cs/ProjectApplication.java
+++ src/main/java/ku/cs/ProjectApplication.java
@@ -13,7 +13,7 @@ public class ProjectApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXRouter.bind(this, stage);
         configRoute();
-        FXRouter.goTo("admin");
+        FXRouter.goTo("signIn");
     }
 
     private static void configRoute() {
@@ -25,6 +25,7 @@ public class ProjectApplication extends Application {
         FXRouter.when("main",packageStr+"main.fxml","Javaverse | main",1024,746);
         FXRouter.when("addreport",packageStr+"report_form.fxml","Javaverse | Report",800,670);
         FXRouter.when("profile",packageStr+"profile.fxml","Javaverse | Profile",600,400);
+        FXRouter.when("myreport",packageStr+"myreport.fxml","Javaverse | MyReport",600,400);
         FXRouter.when("staff",packageStr+"for_staff.fxml","Javaverse | Staff",1024,746);
         FXRouter.when("loginStaff",packageStr+"loginStaff.fxml","Javaverse | Login");
         FXRouter.when("detail",packageStr+"detail.fxml","Javaverse | Detail");
